3ff158fd27da6df1d4c9607297cec29168a65b2e
Evan Sharafuddin <evansharafuddin@wustl.edu>
2025-09-19 18:01:56 -0600

    more debugging

--- src/rawterm_test.cpp
+++ src/rawterm_test.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <string>
 #include "rawterm.h"
 #include "rawterm_utils.h"
 
 
 int main() {
-    std::cout << "hello" << std::endl;
+    
+    
+    std::cout << "Type a key to make stuff happen...\r\n" << std::flush;
+
+    enable_raw_mode();
+
+    char input;
+
+    while ( true ) {
+
+        if ( read( STDIN_FILENO, &input, 1 ) == -1 && errno != EAGAIN )
+            die("read");
+
+        // when here, input has been read
+        out( &input );
+
+    }
+
 }
